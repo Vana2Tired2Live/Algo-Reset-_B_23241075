94ab28449420622c01cc360b585c407e5f38babd
Vana2Tired2Live <vanvilltheartist@gmail.com>
2024-10-29 12:26:03 +0700

    Modul 3 (Kuliah 29 Oktober 2024)

---
+++ Modul 3/02-Tipe Data/Main.py
@@ -0,0 +1,29 @@
+# a = 10, a adalah variabel yang nilainya 10
+
+# Integer
+# Tipe Data : Angka satuan yang tidak mempunyai koma
+
+data_integer = 11
+print("data: ", data_integer)
+print("tipe data : ", type(data_integer))
+# type() berfungsi untuk mengetahui tipe data variabel
+
+# Float
+# Tipe Data : 
+data_float = 3.14
+print("Data : ", data_float)
+print("tipe data : ", type(data_float))
+
+# String
+# Tipe Data : Kumpulan Karakter
+data_string = "Universitas Pendidikan Mandalika"
+print("Data : ", data_string)
+print("tipe data : ", type(data_string))
+
+# Boolean
+# Tipe Data : True/False
+data_boolean = False
+print("Data : ", data_boolean)
+print("tipe data : ", type(data_boolean))
+
+
