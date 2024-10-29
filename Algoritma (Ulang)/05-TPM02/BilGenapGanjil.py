ad7be6ed44f03411af0d7512c1d12f2b63dcde44
Vana2Tired2Live <vanvilltheartist@gmail.com>
2024-10-29 12:23:17 +0700

    Delete Algoritma (Ulang) directory

--- Algoritma (Ulang)/05-TPM02/BilGenapGanjil.py
+++
@@ -1,8 +0,0 @@
-# Memasukkan input User
-bilangan = int(input("Masukkan bilangan: "))
-
-# Memeriksa apakah bilangan genap atau ganjil
-if bilangan % 2 == 0:
-    print(f"{bilangan} adalah bilangan Genap")
-else:
-    print(f"{bilangan} adalah bilangan Ganjil")
--- Algoritma (Ulang)/05-TPM02/KonversiSuhu.py
+++
@@ -1,8 +0,0 @@
-# Memasukkan input suhu dalam Fahrenheit dari User
-fahrenheit = float(input("Masukkan suhu dalam Fahrenheit: "))
-
-# Rumus konversi Fahrenheit ke Celsius
-celsius = (fahrenheit - 32) * 5/9
-
-# Hasil konversi
-print(f"Suhu dalam Celsius: {celsius:.2f}°C")
